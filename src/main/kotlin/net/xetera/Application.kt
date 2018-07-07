41747072c1e3344f5a70da80b1c9c01298f3f802
Unknown <mythologyali@gmail.com>
2018-07-07 04:19:49 -0700

    Added a random number generation command

--- src/main/kotlin/net/xetera/Application.kt
+++ src/main/kotlin/net/xetera/Application.kt
@@ -1,15 +1,27 @@
 package main.kotlin.net.xetera
 
+import main.kotlin.net.xetera.commands.RandomCommand
 import main.kotlin.net.xetera.listeners.PlayerListener
 import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
 class Application : JavaPlugin() {
-    var listeners: Array<Listener> = Array(1, { PlayerListener() })
+    val listeners: Array<Listener> = Array(1, {
+        PlayerListener()
+    })
+
+    val commands:Array<CommandExecutor> = Array(1, {
+        RandomCommand
+    })
+
+
     init {
         Logger.info("Something here")
     }
+
     override fun onEnable() {
         Logger.info("Plugin has started.")
         Logger.info("Adding listeners...")
@@ -17,7 +29,12 @@ class Application : JavaPlugin() {
         listeners.forEach {
             Bukkit.getServer().pluginManager.registerEvents(it, this)
         }
+        this.commands.forEach {
+            this.getCommand("random").executor = RandomCommand
+        }
+    }
 
-
+    override fun onDisable() {
+        Logger.info("Plugin has been stopped.")
     }
 }
--- src/main/kotlin/net/xetera/listeners/PlayerListener.kt
+++ src/main/kotlin/net/xetera/listeners/PlayerListener.kt
@@ -4,14 +4,22 @@ import main.kotlin.net.xetera.Logger
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
 import org.bukkit.event.player.PlayerJoinEvent
 
 class PlayerListener : Listener {
     init {
         Logger.info("Listener initiated")
     }
+
     @EventHandler
-    public fun onPlayerJoin(event: PlayerJoinEvent){
+    fun onPlayerJoin(event: PlayerJoinEvent){
         Bukkit.broadcastMessage("${event.player.displayName} has joined the game!")
     }
+
+    @EventHandler
+    fun onPlayerChat(event: AsyncPlayerChatEvent){
+        Bukkit.broadcastMessage(event.message)
+    }
+
 }
